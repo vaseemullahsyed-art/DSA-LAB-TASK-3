3dad46232de0db75afbe4112af3fe3fec45d158a
vaseemullahsyed-art <vaseemullahsyed@gmail.com>
2026-01-30 11:00:12 +0530

    Add array_sum.c to calculate sum of elements

---
+++ array_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum of elements = %d", sum);
+
+    return 0;
+}
