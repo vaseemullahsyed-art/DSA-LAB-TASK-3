cb671d69fcf6d3a3acba4895e4643019610f1817
vaseemullahsyed-art <vaseemullahsyed@gmail.com>
2026-01-30 11:01:10 +0530

    Add array duplicate count program in C

---
+++ array_duplicate_count.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, count = 0, arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+
+    printf("Total duplicate elements = %d", count);
+
+    return 0;
+}
